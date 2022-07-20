18957c6c36d75d02cf14b9af5b8141dec38bcdee
Amolo <railamolo@gmail.com>
2022-07-20 09:19:50 +0000

    Updated app entrypoint setup script

--- dev/docker/entrypoint.app.sh
+++ dev/docker/entrypoint.app.sh
@@ -10,7 +10,7 @@ else
     composer install
     wait-for-it db:3306 -t 45
     php artisan migrate --database=mysql --force;
-    php artisan key:generate;
+    php artisan key:generate --force;
     chown -R www-data:www-data storage
     exec apache2-foreground
 fi
